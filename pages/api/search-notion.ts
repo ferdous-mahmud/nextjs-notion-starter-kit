f01542a15f181c74ce8a6c0d754511467ce1b6e5
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-04-26 14:01:55 +0600

    first commit

---
+++ pages/api/search-notion.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { search } from '../../lib/notion'
+import * as types from '../../lib/types'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).send({ error: 'method not allowed' })
+  }
+
+  const searchParams: types.SearchParams = req.body
+
+  const results = await search(searchParams)
+
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=60, max-age=60, stale-while-revalidate=60'
+  )
+  res.status(200).json(results)
+}
